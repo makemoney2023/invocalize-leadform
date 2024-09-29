a3a12d03bbacdafad3756607a9f28388ecf26c32
makemoney2023 <124006256+makemoney2023@users.noreply.github.com>
2024-09-28 23:51:44 -0400

    updated supabase variable removed Next Public

--- src/app/api/send-call/route.ts
+++ src/app/api/send-call/route.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { createClient } from '@supabase/supabase-js';
 
 const BLAND_API_KEY = process.env.BLAND_API_KEY;
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
